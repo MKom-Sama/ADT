7e590a26daa71af4ba29707f54cd29a8eef2acfc
MKom <mkomsany3@yahoo.com>
2021-12-24 21:46:23 +0200

    store & retrieve topologies from Memory

--- API/functions.js
+++ API/functions.js
@@ -13,24 +13,36 @@ const readFromJSON = (topologyID) => {
 
 const storeJSONToMem = (topologyID) => {
   // returns True || False
-  const data = readFromJSON(topologyID);
-  if (!data) {
+  const newTopology = readFromJSON(topologyID);
+  if (!newTopology) {
     // Topology JSON not Found
-    console.error("Topology Not Found");
+    console.error("Topology Not Found in JSON");
     return false;
   }
-  // Topology JSON Found
-  fs.writeFile("memory.txt", JSON.stringify(data), (err) => {
+
+  // Get Topologies in Memory
+  let topologies = topologiesInMem();
+  console.log(topologies);
+  // TODO LATER should check if top with same ID is in mem
+  topologies.push(newTopology);
+
+  // Write Topologies to Mem
+
+  fs.writeFile("memory.json", JSON.stringify(topologies), (err) => {
     if (err) {
       throw err;
     }
-    console.log("JSON data is saved.");
+    console.log("Added Topology to Mem");
   });
 };
 
-
+const topologiesInMem = () => {
+  let topologies = fs.readFileSync("memory.json");
+  return JSON.parse(topologies);
+};
 
 module.exports = {
   readFromJSON,
   storeJSONToMem,
+  topologiesInMem,
 };
--- API/index.js
+++ API/index.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 
-const { readFromJSON, storeJSONToMem } = require("./functions");
+const { readFromJSON, storeJSONToMem , topologiesInMem } = require("./functions");
 
-let topologyInJSON = readFromJSON("top1");
+// let topologyInJSON = readFromJSON("top1");
 
-storeJSONToMem("top_1");
-storeJSONToMem("top_2");
+// storeJSONToMem("top_1");
+// storeJSONToMem("top_2");
 
-console.log(topologyInJSON);
+let topologies = topologiesInMem()
+console.log('topInMem',topologies)
+// console.log(topologyInJSON);
